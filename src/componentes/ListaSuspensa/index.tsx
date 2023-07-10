c22e29ffccbf583888328710dc48ac9aa96f8cf3
Luis Henrique <122834024+luissshc29@users.noreply.github.com>
2023-07-10 20:55:27 -0300

    Add files via upload

---
+++ src/componentes/ListaSuspensa/index.tsx
@@ -0,0 +1,25 @@
+import './ListaSuspensa.css'
+
+interface ListaSuspensaProps {
+    label: string
+    aoAlterado: (valor: string) => void
+    valor: string
+    itens: Array<string>
+    obrigatorio: boolean
+}
+
+const ListaSuspensa = (props: ListaSuspensaProps) => {
+    return (
+        <div className='lista-suspensa'>
+            <label>{props.label}</label>
+            <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+                <option value=""></option>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
